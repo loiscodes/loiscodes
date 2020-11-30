0893edfc12b5b4c7c76311478399d5d4b65449d0
Lois Harris <lois@codepush.academy>
2020-11-29 21:29:26 -0500

    remove tweet post for now

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('run')
+const date = Date.now();
+            const getDate = new Date(date);
+            console.log('Github: ', getDate.getHours());
+            console.log('UTC: ', getDate.getUTCHours());
+            console.log('minutes: ', getDate.getMinutes());
+            console.log('Timezone offset: ', getDate.getTimezoneOffset());
